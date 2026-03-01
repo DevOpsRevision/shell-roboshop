7a58afb613180bc298aac2fae97984a2a7ec94c8
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-02-28 22:55:59 -0500

    Creating instances using AWS CLI

--- roboshop.sh
+++ roboshop.sh
@@ -1,21 +1,33 @@
+#!/bin/bash
 AMI_ID=ami-0220d79f3f480ecf5
 SG_ID=sg-0c05c24867a0de439
-ZONE_ID=Z09260871ALCRUTIR75TM
+# ZONE_ID=Z09260871ALCRUTIR75TM  # Unused?
 DOMAIN_NAME=easydevops.fun
 INSTANCES=("frontend" "catalogue" "cart" "payment" "shipping" "user" "dispatch" "rabbitmq" "mongodb" "mysql" "redis")
 
-for instance in ${INSTANCES[@]}
-do
+for instance in "${INSTANCES[@]}"; do
   echo "Creating $instance instance..."
-  INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
+  INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type t2.micro \
+    --security-group-ids "$SG_ID" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query "Instances[0].InstanceId" \
+    --output text)
 
-  if [ $instance != "frontend" ]
-    then
-       IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-       echo "IP address of $instance instance: $IP_ADDRESS"
-    else
-       IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-       echo "Public IP address of $instance instance: $IP_ADDRESS"
+  aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"  # Wait for boot
+
+  if [ "$instance" != "frontend" ]; then
+    IP_ADDRESS=$(aws ec2 describe-instances \
+      --instance-ids "$INSTANCE_ID" \
+      --query "Reservations[0].Instances[0].PrivateIpAddress" \
+      --output text)
+    echo "Private IP of $instance: $IP_ADDRESS"
+  else
+    IP_ADDRESS=$(aws ec2 describe-instances \
+      --instance-ids "$INSTANCE_ID" \
+      --query "Reservations[0].Instances[0].PublicIpAddress" \
+      --output text)
+    echo "Public IP of frontend: $IP_ADDRESS"
   fi
-
 done
