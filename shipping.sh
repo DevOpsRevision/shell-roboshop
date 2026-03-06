c76f789dd5253fca6fbc3bb8bb96a1f68d7da4b7
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-03-06 11:18:03 -0500

    shipping configuration setup

--- shipping.sh
+++ shipping.sh
@@ -74,20 +74,30 @@ VALIDATE $? "Enabling Shipping Service"
 systemctl start shipping &>>$LOG_FILE
 VALIDATE $? "Starting Shipping Service"
 
-# Load the schema
-dnf install mysql -y &>>$LOG_FILE
-VALIDATE $? "Installing MySQL Client"
+mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 -e "use cities"
 
-mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-VALIDATE $? "Loading Shipping Schema"
+if [ $? -ne 0 ]; then
+  echo -e "$Y WARNING :: Shipping schema is not loaded. Loading schema now. $N" | tee -a $LOG_FILE
+  # Load the schema
+  dnf install mysql -y &>>$LOG_FILE
+  VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
-VALIDATE $? "Creating Application User in MySQL"
+  mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+  VALIDATE $? "Loading Shipping Schema"
 
-mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
-VALIDATE $? "Granting Privileges to Application User in MySQL"
+  mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+  VALIDATE $? "Creating Application User in MySQL"
+
+  mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+  VALIDATE $? "Granting Privileges to Application User in MySQL"
+else
+  echo -e "$G INFO :: Shipping schema is already loaded. Skipping schema loading. $N" | tee -a $LOG_FILE
+fi
+
 
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restarting Shipping Service"
 
-
+END_TIME=$(date +%s)
+ELAPSED_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "$G INFO :: Shipping setup completed in $ELAPSED_TIME seconds. $N" | tee -a $LOG_FILE
