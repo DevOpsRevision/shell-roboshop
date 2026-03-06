e1be0a31fdc120aa6abf0c58b68895bc56138eaa
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-03-06 11:13:16 -0500

    payment configuration setup

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -47,3 +47,7 @@ VALIDATE $? "Adding RabbitMQ User"
 
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 VALIDATE $? "Setting RabbitMQ User Permissions"
+
+END_TIME=$(date +%s)
+ELAPSED_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "$G INFO :: Frontend setup completed in $ELAPSED_TIME seconds. $N" | tee -a $LOG_FILE
